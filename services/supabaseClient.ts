5b26612050aeb8bc9ccc7e4422e43d20b5b26e83
peagah10 <paulohenriqueph20010@gmail.com>
2026-01-14 22:24:01 -0300

    Update supabaseClient.ts

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -4,7 +4,7 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-    console.error('Supabase URL and Anon Key are required! Please check your .env file.');
+  throw new Error('Supabase URL and Anon Key are required!');
 }
 
-export const supabase = createClient(supabaseUrl || '', supabaseAnonKey || '');
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
